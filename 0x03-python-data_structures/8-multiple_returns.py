34ccf3884d3e72e0373011621790fdd1c6c68c8b
A. Raphael <raphalg@duck.com>
2022-12-22 20:31:44 +0100

    Fixed failing tests

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,4 +2,4 @@
 
 def multiple_returns(sentence):
     length = len(sentence)
-	return length, None if length == 0 else sentence[0]
+    return length, None if length == 0 else sentence[0]
--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def max_integer(my_list=[]):
-	size = len(my_list)
+    size = len(my_list)
 
     if size == 0:
         return "None"
