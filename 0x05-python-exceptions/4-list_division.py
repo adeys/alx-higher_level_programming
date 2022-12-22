e9c2ac9ca4e304c77988bc86c47e2e317e6a8ba1
A. Raphael <raphalg@duck.com>
2022-12-22 21:46:32 +0100

    Completed task 0x05

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+def list_division(my_list_1, my_list_2, list_length):
+    result_list = []
+
+    for i in range(list_length):
+        result = 0
+        try:
+            result = my_list_1[i] / my_list_2[i]
+        except TypeError:
+            print("wrong type")
+        except ZeroDivisionError:
+            print("division by 0")
+        except IndexError:
+            print("out of range")
+            break
+        finally:
+            result_list.append(result)
+
+    return result_list
---
+++ 0x05-python-exceptions/README.md
@@ -0,0 +1,1 @@
+# Project 0x05: Python - Exceptions
